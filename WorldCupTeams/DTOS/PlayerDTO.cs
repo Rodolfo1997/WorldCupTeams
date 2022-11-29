5f3166702dbdd8979b5bab52e93f7dfec4a4f836
Rodolfo <rodolfostopinto97@gmail.com>
2022-11-29 19:49:06 -0300

    initial commit -m

---
+++ WorldCupTeams/DTOS/PlayerDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace WorldCupTeams.DTOS
+{
+    public class PlayerDTO
+    {
+        public int Key { get; set; }
+        public string Name { get; set; }
+        public string Team { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ WorldCupTeams/Services/IWorldCupService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using WorldCupTeams.DTOS;
+
+namespace WorldCupTeams.Services
+{
+    public interface IWorldCupService
+    {
+        TeamDTO[] FindTeamBy(string Name);
+        TeamDTO[] FindAll();
+        TeamDTO FindTeamBy(int key);
+    }
+}
